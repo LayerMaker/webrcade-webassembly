8ea9691e355f397b9a697d8ab42d16f0b745f416
LayerMaker <fightclubgarmz@gmail.com>
2025-02-16 21:13:01 +0000

    Update feed URL to use relative path

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Function to launch WebRcade
 function launchWebrcade() {
     // Open WebRcade with our local feed URL
-    const feedUrl = 'http://192.168.1.250:8080/feed.json';
+    const feedUrl = window.location.href + 'feed.json';
     window.open(`https://play.webrcade.com/?feed=${encodeURIComponent(feedUrl)}`, '_blank');
 }
 
@@ -29,6 +29,22 @@ window.addEventListener('load', () => {
                 
                 // Add axes count
                 statusText += `<br>Axes: ${gamepad.axes.length}`;
+                
+                // Add button states
+                statusText += '<br><br>Button States:';
+                gamepad.buttons.forEach((button, index) => {
+                    if (button.pressed) {
+                        statusText += `<br>Button ${index}: Pressed (${button.value.toFixed(2)})`;
+                    }
+                });
+                
+                // Add axes states
+                statusText += '<br><br>Axes States:';
+                gamepad.axes.forEach((axis, index) => {
+                    if (Math.abs(axis) > 0.1) { // Add small deadzone
+                        statusText += `<br>Axis ${index}: ${axis.toFixed(2)}`;
+                    }
+                });
                 
                 break;
             }
